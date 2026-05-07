a8654e911c4b710396605326382b8e889c23d654
Ancy <ancy.k.biju@valoriz.com>
2026-05-07 16:45:41 +0400

    Fix terraform binary conflict

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -14,9 +14,9 @@ bash <(curl -fsSL cli.new)
 # Install Terraform
 wget https://releases.hashicorp.com/terraform/1.8.5/terraform_1.8.5_linux_amd64.zip
 
-unzip -o terraform_1.8.5_linux_amd64.zip
+unzip -o terraform_1.8.5_linux_amd64.zip -d terraform-bin
 
-sudo mv terraform /usr/local/bin/
+sudo mv terraform-bin/terraform /usr/local/bin/
 
 # Install Node.js
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
