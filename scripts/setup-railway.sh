1b27ac6fbed406de58202177b02931051ae982f3
Ancy <ancy.k.biju@valoriz.com>
2026-05-08 16:10:16 +0400

    Fix Railway token3

--- scripts/setup-railway.sh
+++ scripts/setup-railway.sh
@@ -2,18 +2,9 @@
 
 set -e
 
-export RAILWAY_TOKEN="${RAILWAY_TOKEN}"
-
 echo "Checking Railway auth..."
 
-if [ -z "$RAILWAY_TOKEN" ]; then
-  echo "RAILWAY_TOKEN is empty"
-  exit 1
-fi
-
-echo "Token exists"
-
-railway whoami
+echo $RAILWAY_TOKEN | railway login --token
 
 PROJECT_ID="637e48f7-b697-4fa3-84ec-fc3d89e5ddda"
 
