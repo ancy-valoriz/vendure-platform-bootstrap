7e095431511513a5c2a0553161461e061b0389d7
Ancy <ancy.k.biju@valoriz.com>
2026-05-11 10:08:37 +0400

    Add Railway environment parameter

--- scripts/setup-railway.sh
+++ scripts/setup-railway.sh
@@ -8,6 +8,8 @@ export RAILWAY_TOKEN=$RAILWAY_TOKEN
 
 PROJECT_ID="637e48f7-b697-4fa3-84ec-fc3d89e5ddda"
 
+ENVIRONMENT="production"
+
 echo "Deploying backend..."
 echo "Token length: ${#RAILWAY_TOKEN}"
 
@@ -16,6 +18,7 @@ cd generated/backend
 railway up \
   --service vendure-backend \
   --project $PROJECT_ID \
+  --environment $ENVIRONMENT \
   --detach
 
 echo "Deploying storefront..."
@@ -25,4 +28,5 @@ cd ../storefront
 railway up \
   --service vendure-storefront \
   --project $PROJECT_ID \
+  --environment $ENVIRONMENT \
   --detach
