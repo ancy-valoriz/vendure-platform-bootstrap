e7a72c51c8c09056b2654dd42eb3225d5a950762
Ancy <ancy.k.biju@valoriz.com>
2026-05-16 22:30:30 +0400

    env added in db url fetch

--- scripts/configure-railway-db.sh
+++ scripts/configure-railway-db.sh
@@ -6,6 +6,10 @@ export PATH="$HOME/.railway/bin:$PATH"
 
 PROJECT_ID="637e48f7-b697-4fa3-84ec-fc3d89e5ddda"
 
+ENVIRONMENT="production"
+
 echo "Fetching Railway variables..."
 
-railway variables --project $PROJECT_ID
+railway variables \
+  --project $PROJECT_ID \
+  --environment $ENVIRONMENT
