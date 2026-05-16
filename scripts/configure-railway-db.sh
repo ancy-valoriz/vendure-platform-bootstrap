5c5df23829567b27ffde3e38146ea98388e811cf
Ancy <ancy.k.biju@valoriz.com>
2026-05-16 23:49:28 +0400

    dburlfix

--- scripts/configure-railway-db.sh
+++ scripts/configure-railway-db.sh
@@ -9,7 +9,7 @@ PROJECT_ID="637e48f7-b697-4fa3-84ec-fc3d89e5ddda"
 ENVIRONMENT="production"
 
 railway variables set \
-  DATABASE_URL="postgresql://..." \
+  DATABASE_URL="postgresql://postgres:GTYjJpWkQbAfciQDbOSOUhZLvlimszhP@postgres.railway.internal:5432/railway" \
   COOKIE_SECRET="XJHe8VoSLDQ6rxGvkBvmcg" \
   SUPERADMIN_USERNAME="admin@vendure.io" \
   SUPERADMIN_PASSWORD="superadmin" \
