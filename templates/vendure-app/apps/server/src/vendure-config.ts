dc8512ff5959398298887cda86a401d8cd1ad461
Ancy <ancy.k.biju@valoriz.com>
2026-05-16 22:22:40 +0400

    dbconfigchange

--- templates/vendure-app/apps/server/src/vendure-config.ts
+++ templates/vendure-app/apps/server/src/vendure-config.ts
@@ -46,12 +46,7 @@ export const config: VendureConfig = {
         synchronize: true,
         migrations: [path.join(__dirname, './migrations/*.+(js|ts)')],
         logging: false,
-        database: process.env.DB_NAME,
-        schema: process.env.DB_SCHEMA,
-        host: process.env.DB_HOST,
-        port: +process.env.DB_PORT,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
+        url: process.env.DATABASE_URL,
     },
     paymentOptions: {
         paymentMethodHandlers: [dummyPaymentHandler],
